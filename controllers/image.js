15a20c4e74200b0a0297428635955e1c841106b9
rpdavila <rafael.pietri@gmail.com>
2021-05-31 22:17:37 -0400

    adding db to heroku 4th try

--- controllers/image.js
+++ controllers/image.js
@@ -10,7 +10,10 @@ const handleApiCall = (req, res) => {
     .then(data => {
         res.json(data);
     })
-    .catch(() => res.status(400).json('Unable to work with API'))
+    .catch(err => {
+        console.log(err)
+        res.status(400).json('Unable to work with API')
+    })
 }
   
 const handleImage = (req, res, db) => {
